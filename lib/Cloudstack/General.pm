6c317e6a5169e776fca61a1ff3820fb400c34585
Heince Kurniawan <heince@gmail.com>
2012-12-04 21:38:16 +0800

    - add timeout config - print out CRITICAL message if can't connect to server / timeout

--- lib/Cloudstack/General.pm
+++ lib/Cloudstack/General.pm
@@ -6,7 +6,7 @@ use XML::LibXML;
 use Config::General;
 
 has 'cfgpath' => (is => "rw", default => '/etc/nagios-cloudstack.conf');
-has [qw/ia iasite site apikey secretkey command flag logger/] => (is => "rw");
+has [qw/ia iasite site apikey secretkey command flag logger timeout/] => (is => "rw");
 
 
 sub getGlobalConfig{
@@ -41,6 +41,7 @@ sub set_connection{
 	$self->site($$conf{url_auth});
 	$self->apikey($$conf{apikey});
 	$self->secretkey($$conf{secretkey});
+	$self->timeout($$conf{timeout} || 10);
 }
 
 sub set_perlssl{
@@ -75,6 +76,7 @@ sub get_result{
 	$self->logger->debug("apikey = " . $self->apikey) if defined $self->apikey;
 	$self->logger->debug("secretkey = " . $self->secretkey) if defined $self->secretkey;
 	$self->logger->debug("command = " . $self->command) if defined $self->command;
+	$self->logger->debug("timeout = " . $self->timeout) if defined $self->timeout;
 	
 	my ($field,$value);
 	my $my_filename = basename($0, '');
@@ -130,13 +132,20 @@ sub get_result{
 	
 	### get URL ###
 	$self->logger->debug("Getting response from the $url\n");
-	my $mech = WWW::Mechanize->new(autocheck => 1);
+	my $mech = WWW::Mechanize->new(autocheck => 1, timeout => $self->timeout);
 	my $temp;
-	eval {  $temp = $mech->get($url); };
+	eval {  
+		$temp = $mech->get($url); 
+	};
 	
 	if($@){
 		my $resp = $mech->response();
 		
+		unless($resp->is_success){
+			print "CRITICAL : " . $resp->status_line . "\n";
+			exit 2;
+		}
+
 		my $xml = XML::LibXML->load_xml(string => $resp->decoded_content);
 		my $errcode = "Error code: " . $xml->findnodes("/*/errorcode")->string_value() . "\n" if $xml;
 		my $errtxt = "Error text: " . $xml->findnodes("/*/errortext")->string_value() . "\n" if $xml;
